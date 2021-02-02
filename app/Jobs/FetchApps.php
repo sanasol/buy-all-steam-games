0c9dc0be7f3bcd53180077f72d6a723ea2367613
sanasol <mail@sanasol.ws>
2021-02-02 05:41:36 +0300

    up ru

--- app/Jobs/FetchApps.php
+++ app/Jobs/FetchApps.php
@@ -11,6 +11,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 
 class FetchApps implements ShouldQueue
 {
@@ -37,44 +38,63 @@ class FetchApps implements ShouldQueue
      */
     public function handle(Client $client)
     {
-        $json = json_decode($client->get('http://store.steampowered.com/api/appdetails/', [
-            'query' => [
-                'appids' => $this->appids,
-                'cc' => config('steam.country'),
-                'l' => config('steam.language'),
-                'v' => 1,
-                'filters' => 'price_overview',
-            ],
-            'proxy' => $this->proxy,
-            'connect_timeout' => 5,
-            'read_timeout' => 5,
-        ])->getBody(), true);
+        try {
+            $json = json_decode($client->get('http://store.steampowered.com/api/appdetails/', [
+                'query' => [
+                    'appids' => $this->appids,
+                    'cc' => config('steam.country'),
+                    'l' => config('steam.language'),
+                    'v' => 1,
+                    'filters' => 'price_overview',
+                ],
+                'proxy' => $this->proxy,
+                'connect_timeout' => 5,
+                'read_timeout' => 5,
+            ])->getBody(), true);
 
-        $results = collect($json);
+            $results = collect($json);
 
-        $original = 0;
-        $sale = 0;
-        foreach ($results as $result) {
-            if (!isset($result['data']['price_overview'])) {
-                continue;
+            $original = 0;
+            $sale = 0;
+            foreach ($results as $result) {
+                if (!isset($result['data']['price_overview'])) {
+                    continue;
+                }
+
+                $original += $result['data']['price_overview']['initial'] / 100;
+                $sale += $result['data']['price_overview']['final'] / 100;
             }
 
-            $original += $result['data']['price_overview']['initial'] / 100;
-            $sale += $result['data']['price_overview']['final'] / 100;
+            Record::where('cc', config('steam.country'))
+                ->where('language', config('steam.language'))
+                ->where('created_at', '>=', today())
+                ->increment('original', $original);
+
+            Record::where('cc', config('steam.country'))
+                ->where('language', config('steam.language'))
+                ->where('created_at', '>=', today())
+                ->increment('sale', $sale);
+
+            Cache::pull('view');
+
+            usleep(500000);
+
+        } catch (\Exception $e) {
+
+            Log::error('Proxy error?', ['message' => $e]);
+            $proxies = [];
+
+            foreach(range(3001, 3021) as $port) {
+                $proxies[] = config('steam.proxy1').$port;
+            }
+
+            foreach(range(30011, 30045) as $port) {
+                $proxies[] = config('steam.proxy2').$port;
+            }
+
+            $proxy = $proxies[array_rand($proxies)];
+
+            dispatch(new FetchApps($this->appids, $proxy));
         }
-
-        Record::where('cc', config('steam.country'))
-            ->where('language', config('steam.language'))
-            ->where('created_at', '>=', today())
-            ->increment('original', $original);
-
-        Record::where('cc', config('steam.country'))
-            ->where('language', config('steam.language'))
-            ->where('created_at', '>=', today())
-            ->increment('sale', $sale);
-
-        Cache::pull('view');
-
-        usleep(500000);
     }
 }
